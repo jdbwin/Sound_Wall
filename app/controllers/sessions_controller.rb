cd825b78be6a0b5ad1843500a920544205c106de
jdbwin <dbwin7@gmail.com>
2016-01-08 12:31:58 -0500

    cleaned comments

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,16 +5,14 @@ class SessionsController < ApplicationController
 
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      #redirect_to movies_path, notice: "Welcome back, #{user.firstname}!"
     else
       flash.now[:alert] = "Log in failed..."
-      #render :new
+      render :new
     end
   end
 
   def destroy
     session[:user_id] = nil
-    #redirect_to movies_path, notice: "Adios!"
   end
 
 end
